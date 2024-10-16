3adf9b5f002874ad4dd84abd3266280b6af10da9
Ilham <142871024+IlhamIslahuddin@users.noreply.github.com>
2024-10-16 23:05:44 +0800

    Create 27. Remove Element.py

---
+++ 27. Remove Element.py
@@ -0,0 +1,9 @@
+class Solution:
+    def removeElement(self, nums: List[int], val: int) -> int:
+        count = 0
+        while count < len(nums):
+            if nums[count] == val:
+                nums.pop(count)
+            else:
+                count += 1
+        print (nums)
