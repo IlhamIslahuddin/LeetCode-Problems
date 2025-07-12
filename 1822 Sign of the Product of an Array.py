c45c8108a2df1d20a402990e67730746d4feb3db
Ilham <142871024+IlhamIslahuddin@users.noreply.github.com>
2025-07-12 19:36:53 +0800

    Create 1822 Sign of the Product of an Array.py

---
+++ 1822 Sign of the Product of an Array.py
@@ -0,0 +1,11 @@
+class Solution:
+    def arraySign(self, nums: List[int]) -> int:
+        x = 1
+        for num in nums:
+            x = x * num
+        if x > 0:
+            return 1
+        elif x < 0:
+            return -1
+        else:
+            return 0
